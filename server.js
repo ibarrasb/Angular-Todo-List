e225390d189c3ac0089c93974247548860c963a0
Eduardo Ibarra <eddieibarra43@gmail.com>
2021-02-10 02:30:13 -0600

    fixed bugs

--- server.js
+++ server.js
@@ -2,13 +2,22 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/Angular-Todo-List'));
+app.use(express.static('./dist/angular-app'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/Angular-Todo-List/'}),
+    res.sendFile('index.html', {root: 'dist/angular-app/'}),
 );
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+
+
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
